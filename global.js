c85c41dfa6112ec9a6f605eebc7d973c9bda7b05
Rachel <rmshinski@gmail.com>
2021-05-18 16:29:59 -0400

    518 hw

--- global.js
+++ global.js
@@ -9,3 +9,5 @@ const int = setInterval (() => {
     console.log('in the interval');
 }, 1000);
 
+console.log(__dirname);
+console.log(__filename);
---
+++ people.js
@@ -0,0 +1,9 @@
+const people = ['yoshi', 'ryu', 'chun-li', 'mario'];
+const ages = [25, 32, 10, 99]
+
+// console.log(people);
+
+module.exports = {
+    people,
+    ages
+};
